f1f41ccb44db4d8f158731b9f5073df8e95f0295
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-03-03 22:43:09 +0800

    Apply nixpkgs-fmt

--- pkgs/rime-packages/with-rime-deps/default.nix
+++ pkgs/rime-packages/with-rime-deps/default.nix
@@ -6,4 +6,4 @@ let
   inherit (lib.lists) map unique flatten;
   withDeps = p: [ p ] ++ p.rimeDependencies;
 in
-  unique (flatten (map withDeps rimePackages))
+unique (flatten (map withDeps rimePackages))
