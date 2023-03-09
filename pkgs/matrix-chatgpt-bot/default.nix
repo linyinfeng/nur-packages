b507105d243cf979c23b59e79ff2497b1adcb97c
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-03-09 17:05:06 +0800

    matrix-chatgpt-bot: init at v3.0.0

---
+++ pkgs/matrix-chatgpt-bot/default.nix
@@ -0,0 +1,33 @@
+{ sources, lib, mkYarnPackage, nodejs, makeWrapper, matrix-sdk-crypto-nodejs }:
+
+mkYarnPackage {
+  inherit (sources.matrix-chatgpt-bot) pname version src;
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    yarn --offline tsc
+    runHook postBuild
+  '';
+
+  postInstall = ''
+    out_node_path="$out/libexec/matrix-chatgpt-bot/node_modules"
+
+    rm -r "$out_node_path/@matrix-org/matrix-sdk-crypto-nodejs"
+    ln -s "${matrix-sdk-crypto-nodejs}/lib/node_modules/@matrix-org/matrix-sdk-crypto-nodejs" "$out_node_path/@matrix-org"
+
+    makeWrapper ${nodejs}/bin/node "$out/bin/matrix-chatgpt-bot" \
+      --add-flags "$out_node_path/matrix-chatgpt-bot/dist/index.js" \
+      --prefix NODE_PATH : "$out_node_path"
+  '';
+
+  meta = with lib; {
+    description = "Talk to ChatGPT via any Matrix client";
+    homepage = "https://github.com/matrixgpt/matrix-chatgpt-bot";
+    license = licenses.agpl3;
+    maintainers = with maintainers; [ yinfeng ];
+  };
+}
