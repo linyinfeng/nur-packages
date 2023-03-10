e5b76bed7c9b1603a63ef29582cf0595ca2ada0e
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-03-10 20:31:00 +0800

    Adopt flake-parts

---
+++ flake/packages.nix
@@ -0,0 +1,11 @@
+{ self, inputs, ... }:
+
+{
+  perSystem = { self', pkgs, ... }:
+    {
+      legacyPackages = self.lib.makePackages pkgs ../pkgs {
+        selfLib = self.lib;
+      };
+      packages = inputs.flake-utils.lib.flattenTree (self'.legacyPackages);
+    };
+}
