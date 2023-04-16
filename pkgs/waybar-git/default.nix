064706bce0c78e16729b9604ef05ad1f041606eb
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-04-16 19:42:17 +0800

    Mark waybar-git borken on stable

--- pkgs/waybar-git/default.nix
+++ pkgs/waybar-git/default.nix
@@ -1,5 +1,9 @@
-{ sources, waybar }:
+{ sources, waybar, lib }:
 
 waybar.overrideAttrs (old: {
   inherit (sources.waybar-git) version src;
+
+  meta = old.meta // {
+    broken = with lib; !(versionAtLeast (versions.majorMinor trivial.version) "23.05");
+  };
 })
