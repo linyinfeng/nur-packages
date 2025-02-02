25a86e0e1e678984b3c7a233df9595d7e62b85a2
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-02-02 16:09:51 +0800

    lib/make-apps: add meta.description

--- lib/make-apps.nix
+++ lib/make-apps.nix
@@ -7,10 +7,12 @@ let
     p: drv:
     lib.mapAttrsToList (
       appName: exec:
-      lib.nameValuePair appName {
+      lib.nameValuePair appName ({
         type = "app";
         program = "${drv}/bin/${exec}";
-      }
+      } // lib.optionalAttrs (drv ? meta.description) {
+        meta.description = drv.meta.description;
+      })
     ) (appNames p);
 in
 lib.listToAttrs (lib.flatten (lib.mapAttrsToList mkAppsSingle packages))
