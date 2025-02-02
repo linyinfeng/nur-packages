246ccbcd0059fcfc81f7d9505f66a1cf846860d7
Lin Yinfeng <lin.yinfeng@outlook.com>
2025-02-02 16:12:33 +0800

    lib/make-apps: apply nix fmt

--- lib/make-apps.nix
+++ lib/make-apps.nix
@@ -7,12 +7,15 @@ let
     p: drv:
     lib.mapAttrsToList (
       appName: exec:
-      lib.nameValuePair appName ({
-        type = "app";
-        program = "${drv}/bin/${exec}";
-      } // lib.optionalAttrs (drv ? meta.description) {
-        meta.description = drv.meta.description;
-      })
+      lib.nameValuePair appName (
+        {
+          type = "app";
+          program = "${drv}/bin/${exec}";
+        }
+        // lib.optionalAttrs (drv ? meta.description) {
+          meta.description = drv.meta.description;
+        }
+      )
     ) (appNames p);
 in
 lib.listToAttrs (lib.flatten (lib.mapAttrsToList mkAppsSingle packages))
